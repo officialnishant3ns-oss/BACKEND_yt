8d776b1bc48f2de5a1c0a1e0af5e129e7485bbaa
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-10-27 18:06:54 +0530

    worked on midleware and utiles

---
+++ src/Utils/fileUpload.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from 'cloudinary'
+
+import fs from 'fs'
+import path from 'path'
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_KEY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+const uploadonclodinary = async (localFilePath) => {
+    try {
+          if (!localFilePath) {
+      console.log(" File path not provided")
+      return null
+    }
+            const response = await cloudinary.uploader.upload(localFilePath, {
+                resource_type: 'auto'
+            })
+            console.log("file is uploaded on clodinary", response.url)
+            return response
+        
+    } catch (error) {
+
+   fs.unlinkSync(localFilePath)
+   return null
+    }
+}
+
+
+export default uploadonclodinary
